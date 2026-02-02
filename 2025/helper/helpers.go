af7b0dec77e34a1c4707db08e716d481cc51dcd3
paxmix <manh1998nb@gmail.com>
2026-02-02 15:05:15 +0700

    add Run() helper function to easily get solution

--- 2025/helper/helpers.go
+++ 2025/helper/helpers.go
@@ -9,6 +9,11 @@ import (
 	"os"
 	"strings"
 
+	day1 "aoc/2025/solutions/day_1"
+	day2 "aoc/2025/solutions/day_2"
+	day3 "aoc/2025/solutions/day_3"
+	day4 "aoc/2025/solutions/day_4"
+
 	"github.com/joho/godotenv"
 )
 
@@ -47,4 +52,41 @@ func FetchData(day int) string {
 	}
 
 	return strings.TrimSpace(string(body))
+}
+
+var solutions = []struct {
+	Part1 func(string)
+	Part2 func(string)
+	TEST  string
+}{
+	1: {day1.Part1, day1.Part1, day1.TEST},
+	2: {day2.Part1, day2.Part2, day2.TEST},
+	3: {day3.Part1, day3.Part2, day3.TEST},
+	4: {day4.Part1, day4.Part2, day4.TEST},
+}
+
+func Run(day, part int, isTest bool) {
+	if day < 0 || day >= len(solutions) {
+		log.Fatal("\n-> invalid day! <-")
+	}
+	solution := solutions[day]
+	if day == 1 && part == 1 {
+		fmt.Println("Day 1 only have part 2")
+		part = 2
+	}
+	var input string
+	if isTest {
+		input = solution.TEST
+	} else {
+		input = FetchData(day)
+	}
+
+	switch part {
+	case 1:
+		solution.Part1(input)
+	case 2:
+		solution.Part2(input)
+	default:
+		fmt.Print("Each day only has two parts")
+	}
 }
