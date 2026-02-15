371e7820af397cbbb13209a23eacdedaadf9017b
vostro <captain.obvious.hd6@gmail.com>
2026-02-15 17:06:49 +0700

    remove godotenv

--- 2025/helpers.go
+++ 2025/helpers.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
@@ -13,46 +11,7 @@ import (
 	day3 "aoc/2025/day_3"
 	day4 "aoc/2025/day_4"
 	day5 "aoc/2025/day_5"
-
-	"github.com/joho/godotenv"
 )
-
-func FetchData(day int) string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Warning: .env file not found, using environment variables")
-	}
-
-	sessionToken := os.Getenv("AOC_SESSION")
-	if sessionToken == "" {
-		log.Fatal("AOC_SESSION not set in environment")
-	}
-
-	url := fmt.Sprintf("https://adventofcode.com/2025/day/%d/input", day)
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Cookie", fmt.Sprintf("session=%s", sessionToken))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatalf("Failed to fetch input: %v", err)
-	}
-	defer func() {
-		if closeErr := resp.Body.Close(); closeErr != nil {
-			log.Printf("Failed to close response body: %v", closeErr)
-		}
-	}()
-
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Failed to fetch input: status %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Failed to read response: %v", err)
-	}
-
-	return strings.TrimSpace(string(body))
-}
 
 func getInput(file string) string {
 	data, err := os.ReadFile(file)
@@ -63,9 +22,9 @@ func getInput(file string) string {
 }
 
 var solutions = []struct {
-	Part1 func(string)
-	Part2 func(string)
-	TEST  string
+	part1 func(string)
+	part2 func(string)
+	test  string
 }{
 	1: {day1.Part1, day1.Part1, day1.TEST},
 	2: {day2.Part2, day2.Part2, day2.TEST},
@@ -84,17 +43,17 @@ func Run(day, part int, isTest bool) {
 		part = 2
 	}
 	var input string
-	file := fmt.Sprintf("day_%d/puzzle.txt", day)
+	file := fmt.Sprintf("puzzles/day_%d.txt", day)
 	if isTest {
-		input = solution.TEST
+		input = solution.test
 	} else {
 		input = getInput(file)
 	}
 	switch part {
 	case 1:
-		solution.Part1(input)
+		solution.part1(input)
 	case 2:
-		solution.Part2(input)
+		solution.part2(input)
 	default:
 		fmt.Println("Each day only has two parts")
 	}
